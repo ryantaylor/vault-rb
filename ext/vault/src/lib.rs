8d6155af7ad560b078bc861f14e574928ed9a2a1
ryantaylor <2320507+ryantaylor@users.noreply.github.com>
2023-03-16 12:34:51 -0400

    Add rudimentary tick parsing and length calculation.

--- ext/vault/src/lib.rs
+++ ext/vault/src/lib.rs
@@ -20,6 +20,7 @@ pub mod replay;
 pub mod span;
 pub mod player;
 pub mod item;
+pub mod tick;
 
 fn hello(subject: String) -> String {
     format!("Hello from Rust, {}!", subject)
--- ext/vault/src/replay.rs
+++ ext/vault/src/replay.rs
@@ -2,6 +2,8 @@ use serde::{Serialize, Deserialize};
 use std::slice::Chunks;
 use nom::{
     combinator::map};
+use nom::combinator::eof;
+use nom::multi::many_till;
 use nom::sequence::tuple;
 use nom_tracable::tracable_parser;
 use crate::chunky::Chunky;
@@ -9,13 +11,15 @@ use crate::chunks::Chunk;
 
 use crate::header::Header;
 use crate::span::{ParserResult, Span};
+use crate::tick::Tick;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[magnus::wrap(class = "Vault::Replay")]
 pub struct Replay {
     pub header: Header,
     pub chunkies: Vec<Chunky>,
-    pub chunks: Vec<Chunk>
+    pub chunks: Vec<Chunk>,
+    pub length: usize
 }
 
 impl Replay {
@@ -28,7 +32,8 @@ impl Replay {
                 Chunk::parse_chunk,
                 Chunky::parse_chunky,
                 Chunk::parse_chunk,
-                Chunk::parse_chunk
+                Chunk::parse_chunk,
+                many_till(Tick::parse_tick, eof)
             )),
             |(
                 header,
@@ -36,12 +41,14 @@ impl Replay {
                 foldpost_chunk,
                 second_chunky,
                 foldinfo_chunk,
-                datasdsc_chunk
+                datasdsc_chunk,
+                (ticks, _)
              )| {
                 Replay {
                     header,
                     chunkies: vec![first_chunky, second_chunky],
-                    chunks: vec![foldpost_chunk, foldinfo_chunk, datasdsc_chunk]
+                    chunks: vec![foldpost_chunk, foldinfo_chunk, datasdsc_chunk],
+                    length: ticks.len() / 8
                 }
             }
         )(input)
