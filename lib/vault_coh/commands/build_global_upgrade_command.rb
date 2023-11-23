af5940e9eccf4cac0879688a88641ef990e5c145
ryantaylor <2320507+ryantaylor@users.noreply.github.com>
2023-11-23 10:15:15 -0500

    Additional documentation.

---
+++ lib/vault_coh/commands/build_global_upgrade_command.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module VaultCoh
+  module Commands
+    # Wrapper for an upgrade construction command.
+    class BuildGlobalUpgradeCommand < Command
+    end
+  end
+end
