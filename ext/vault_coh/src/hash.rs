caa0f9fc60b05faeda7f5985785b94980584c6c2
ryantaylor <2320507+ryantaylor@users.noreply.github.com>
2024-06-03 13:21:20 -0400

    Fix hash keys not being symbols.

--- ext/vault_coh/src/hash.rs
+++ ext/vault_coh/src/hash.rs
@@ -1,4 +1,4 @@
-use magnus::{value::ReprValue, RHash, RString, Value};
+use magnus::{value::ReprValue, RHash, RString, Symbol, Value};
 use vault::{Command, Map, Message, Player, Replay};
 
 pub trait HashExt {
@@ -34,67 +34,86 @@ impl HashExt for Command {
         match self {
             Command::BuildGlobalUpgrade(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("BuildGlobalUpgrade"))
+                hash.aset(Symbol::new("type"), RString::new("BuildGlobalUpgrade"))
                     .unwrap();
-                hash.aset("action_type", RString::new("CMD_Upgrade"))
+                hash.aset(Symbol::new("action_type"), RString::new("CMD_Upgrade"))
                     .unwrap();
                 hash.as_value()
             }
             Command::BuildSquad(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("BuildSquad")).unwrap();
-                hash.aset("action_type", RString::new("CMD_BuildSquad"))
+                hash.aset(Symbol::new("type"), RString::new("BuildSquad"))
+                    .unwrap();
+                hash.aset(Symbol::new("action_type"), RString::new("CMD_BuildSquad"))
                     .unwrap();
                 hash.as_value()
             }
             Command::CancelConstruction(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("CancelConstruction"))
+                hash.aset(Symbol::new("type"), RString::new("CancelConstruction"))
                     .unwrap();
-                hash.aset("action_type", RString::new("CMD_CancelConstruction"))
-                    .unwrap();
+                hash.aset(
+                    Symbol::new("action_type"),
+                    RString::new("CMD_CancelConstruction"),
+                )
+                .unwrap();
                 hash.as_value()
             }
             Command::CancelProduction(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("CancelProduction")).unwrap();
-                hash.aset("action_type", RString::new("CMD_CancelProduction"))
+                hash.aset(Symbol::new("type"), RString::new("CancelProduction"))
                     .unwrap();
+                hash.aset(
+                    Symbol::new("action_type"),
+                    RString::new("CMD_CancelProduction"),
+                )
+                .unwrap();
                 hash.as_value()
             }
             Command::SelectBattlegroup(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("SelectBattlegroup"))
+                hash.aset(Symbol::new("type"), RString::new("SelectBattlegroup"))
                     .unwrap();
-                hash.aset("action_type", RString::new("PCMD_InstantUpgrade"))
-                    .unwrap();
+                hash.aset(
+                    Symbol::new("action_type"),
+                    RString::new("PCMD_InstantUpgrade"),
+                )
+                .unwrap();
                 hash.as_value()
             }
             Command::SelectBattlegroupAbility(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("SelectBattlegroupAbility"))
-                    .unwrap();
-                hash.aset("action_type", RString::new("PCMD_TentativeUpgrade"))
-                    .unwrap();
+                hash.aset(
+                    Symbol::new("type"),
+                    RString::new("SelectBattlegroupAbility"),
+                )
+                .unwrap();
+                hash.aset(
+                    Symbol::new("action_type"),
+                    RString::new("PCMD_TentativeUpgrade"),
+                )
+                .unwrap();
                 hash.as_value()
             }
             Command::Unknown(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("Unknown")).unwrap();
+                hash.aset(Symbol::new("type"), RString::new("Unknown"))
+                    .unwrap();
                 hash.as_value()
             }
             Command::UseAbility(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("UseAbility")).unwrap();
-                hash.aset("action_type", RString::new("CMD_Ability"))
+                hash.aset(Symbol::new("type"), RString::new("UseAbility"))
+                    .unwrap();
+                hash.aset(Symbol::new("action_type"), RString::new("CMD_Ability"))
                     .unwrap();
                 hash.as_value()
             }
             Command::UseBattlegroupAbility(data) => {
                 let hash: RHash = serde_magnus::serialize(data).unwrap();
-                hash.aset("type", RString::new("UseBattlegroupAbility"))
+                hash.aset(Symbol::new("type"), RString::new("UseBattlegroupAbility"))
                     .unwrap();
-                hash.aset("action_type", RString::new("PCMD_Ability"))
+                hash.aset(Symbol::new("action_type"), RString::new("PCMD_Ability"))
                     .unwrap();
                 hash.as_value()
             }
